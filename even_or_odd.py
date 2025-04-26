60862eb9961ec20c7a9f68fe7e49b42f4f4fa818
Ndolo Muindi <benson.ndolo@strathmore.edu>
2025-04-26 23:05:39 +0300

    Create even_or_odd.py

---
+++ even_or_odd.py
@@ -0,0 +1,12 @@
+def even_or_odd(num):
+  
+#check whether if when the number is divided by two, the remainder is 0
+  if num % 2 == 0:
+    
+#if true it'll return even
+    return "even"
+
+# if false it'll return odd
+  else:
+    return "odd"
+  
