1f547d41ab483fbc8fc1858c20cfe8a14f0671e3
Ndolo Muindi <benson.ndolo@strathmore.edu>
2025-04-26 23:37:42 +0300

    Create factorial_recursive.py

---
+++ factorial_recursive.py
@@ -0,0 +1,12 @@
+def factorial_recursive(y):
+
+# check whether y is equal to 0 or 1
+  if y == 0 or y == 1:
+
+# establish as the base case since factorial of 0 or 1 is 1
+    return 1
+    
+  else:
+    
+# and the recursive case to multiply y by y-1 until it is equal to 1
+    return n * factorial_recursive(n-1)
