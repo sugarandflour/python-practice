128227c4da69124911a3cbf56c87ae3f3e03c935
Ndolo Muindi <benson.ndolo@strathmore.edu>
2025-04-26 22:57:53 +0300

    Create sum.py

---
+++ sum.py
@@ -0,0 +1,12 @@
+# create the list with the numbers
+def sum_list(numbers_in_list):
+  
+#create a variable total and initialize it to 0
+  total= 0
+  
+# loop through the numbers in the list and add it to the total
+  for num in numbers_in_list:
+    total += num
+    
+#return the final value  
+  return total
